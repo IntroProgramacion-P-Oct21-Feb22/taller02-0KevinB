a88d26f4d979bbf92353cda4fe123cd9df0f5af2
Kevin Barrazueta <kabarrazueta@utpl.edu.ec>
2021-10-22 11:20:12 -0500

    primer commit

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author Usuario iTC
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos basico de Java");
+        System.out.println("Versionado a traves de Git");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Ademas de usar GitHub Classrom");
+    }
+    
+}
